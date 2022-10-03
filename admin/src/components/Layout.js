c52be7d391622eb676a2f52a2b62934bb99f95ee
owen254 <owenmiash90@gmail.com>
2022-10-03 17:08:27 +0300

    working on sidebar

--- admin/src/components/Layout.js
+++ admin/src/components/Layout.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import  {Outlet} from "react-router-dom"
+import Sidebar from './Sidebar'
 // import "../app.css"
  function Layout() {
   return (
     <main className='app'>
+      <Sidebar/>
        <Outlet/>
     </main>
     
---
+++ admin/src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./sidebar.css"
+function Sidebar() {
+  return (
+    <div className='side-bar'>
+
+    </div>
+  )
+}
+
+export default Sidebar
